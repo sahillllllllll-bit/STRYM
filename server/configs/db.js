dd0088228c4b0ef3a27411bfd7933440875d58d6
sahillllllllll-bit <sahilsingh59443@gmail.com>
2025-09-15 20:31:36 +0530

    mongodb Error solved

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB =async () => {
     try {
         mongoose.connection.on('connected',()=> console.log('Database Connected'));
-        await mongoose.connect(`${process.env.MONGODB_URL}/Strym`)
+        await mongoose.connect(process.env.MONGODB_URL)
     } catch (error) {
         console.log(error.message);
     }
